037d2c1f98613bf7300e7f12bad06927dd0d73ef
Swolebrain <thecodingteacher@gmail.com>
2016-10-03 18:11:40 -0400

    docs: adding spanish translation to test 2.1

--- test2.1.js
+++ test2.1.js
@@ -25,7 +25,7 @@ for (var i = 0; i < arr.length; i++){
 /*Write a function which receives two inputs: a variable x and an array.
 it should return true whenever the variable x is present in the array
 and false otherwise.
-Escribe una function que recibe dos parametros: una variable x y un arreglo. Debe retornar verdadero cuando la variable x esta presente en el arreglo y falso de otra manera.
+Escribe una function que recibe dos parametros: una variable x y un arreglo.  Debe retornar verdadero cuando la variable x esta presente en el arreglo y falso de otra manera.
 */
 function findX(arr, x){
 
@@ -33,9 +33,11 @@ function findX(arr, x){
 
 
 
-//write a function getAvg.
-//It should return the average of the values of the elements in the array.
-//The average is the sum of the elements divided by the number of elements.
+/*Write a function getAvg.
+It should return the average of the values of the elements in the array.
+The average is the sum of the elements divided by the number of elements.
+Escribe una function getAvg. Debe retornar el promedio (Average) de los elementos en el arreglo que recibe como parametro.
+*/
 function getAvg(arr){
 
 }
@@ -43,49 +45,55 @@ function getAvg(arr){
 
 
 
-//Write the statement that you would use if you wanted to use jQuery to select
-//the header (containing the navigation bar and search box) of microsoft.com
+/*Write the statement that you would use if you wanted to use jQuery to select the header (containing the navigation bar and search box) of microsoft.com
+Escribe el comando que utilizarias si quisieras usar jQuery para seleccionar el header (donde esta la navegacion y la barra de busqueda) de microsoft.com
+*/
+
+
+
+/*Write the jQuery statement you would use in order to select the statement that reads "Technology Programs" at tech.fvi.edu
+Escribe el comando de jQuery que utilizarias para seleccionar el titulo que dice "Technology Programs" en tech.fvi.edu*/
 
 
 
 
-//Write the jQuery statement you would use in order to select the statement that reads
-//"Technology Programs" at tech.fvi.edu
 
 
-
-
-
-
-//Write a complete jQuery statement which can attach an event listener to the button below
+/*Write a complete jQuery statement which can attach an event listener to the button below
+Escribe un mini-programa en jQuery que sirva para anexar un manejador de eventos al siguiente boton
+*/
 <button id="el-boton">Aprietame</button>
 
 
 
 
-//If you wanted the above button to send a post request to a server, which command would you
-//need? Where would you need to write it?
+/*If you wanted the above button to send a post request to a server, which command would you need? Where would you need to write it?
+Si quisieras que ese boton mandara un request a un servidor, que comando puedes usar? Donde lo pondrias? */
+
+
+
+
+/*Write a function that prints "lonely pizza" to the console, and then call the function
+Escribe una function que ponga el mensaje "Lonely pizza" en la consola y luego invoca tu funcion
+*/
 
 
 
 
 
-//Write a function that prints "lonely pizza" to the console, and then call the function
 
-
-
-
-
-
-// Write a for loop that iterates through every values in nums
-// and prints each value to the console.
+/* Write a for loop that iterates through every values in nums
+and prints each value to the console.
+Escribe un loop que pase por cada uno de estos elementos e imprima cada valor a la consola.
+*/
 var nums = [1, 2, 3, 5, 7, 8, 10, 12, 45, 66, 87, 98];
 
 
 
 
-// Using the specific array function that adds data to the back end of the
-// array, add the next number to your newly corrected list
+/* Using the specific array function that adds data to the back end of the array, add the next number to your newly corrected list
+Utilizando la funcion de los arreglos que adiciona datos al final del arreglo, adiciona el siguiente numero de la secuencia.
+*/
 var list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
