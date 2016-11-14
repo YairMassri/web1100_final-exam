3ed3e83e88e3f8d101debf278d92c8a8f1df3692
Brenton Cozby <brenton.cozby@gmail.com>
2016-11-14 18:16:40 -0500

    improve enemy movement

--- Exercises/canvas-game-complete/js/main.js
+++ Exercises/canvas-game-complete/js/main.js
@@ -1,9 +1,6 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-
-
-
 function Player(x, y) {
     this.x = x;
     this.y = y;
@@ -14,25 +11,25 @@ function Player(x, y) {
     this.avatar = "";
 }
 
-Player.prototype.update = function(){
-    if(this.x > 590) {
-      this.x = -20;
+Player.prototype.update = function() {
+    if (this.x > 590) {
+        this.x = -20;
     }
-    if(this.y > 590) {
-      this.y = -20;
+    if (this.y > 590) {
+        this.y = -20;
     }
-    if(this.x < -20) {
-      this.x = 590;
+    if (this.x < -20) {
+        this.x = 590;
     }
-    if(this.y < -20) {
-      this.y = 590;
+    if (this.y < -20) {
+        this.y = 590;
     }
 
     this.x += this.velX;
     this.y += this.velY;
 }
 
-Player.prototype.render = function(){
+Player.prototype.render = function() {
     ctx.strokeStyle = "#000";
     ctx.strokeRect(this.x, this.y, this.width, this.height);
 }
@@ -45,46 +42,44 @@ var player1 = new Player(50, 50);
 
 
 function Enemy(x, y) {
-  this.x = x;
-  this.y = y;
-  this.width = 40;
-  this.height = 40;
-  this.velX = -1 + Math.random() * 2;
-  this.velY = -1 + Math.random() * 2;
-  this.avatar = document.getElementById("trump");
-  this.ticks = 0;
+    this.x = x;
+    this.y = y;
+    this.width = 40;
+    this.height = 40;
+    this.velX = -2 + Math.random() * 4;
+    this.velY = -2 + Math.random() * 4;
+    this.avatar = document.getElementById("trump");
+    this.ticks = 0;
 }
 
 Enemy.prototype.update = function() {
-  this.ticks++;
-  var self = this;
-  if(this.x > 560) {
-    this.x = 560;
-  }
-  if(this.y > 560) {
-    this.y = 560;
-  }
-  if(this.x < 0) {
-    this.x = 0;
-  }
-  if(this.y < 0) {
-    this.y = 0;
-  }
+    this.ticks++;
+    var self = this;
+    if (this.x > 590) {
+        this.x = -20;
+    }
+    if (this.y > 590) {
+        this.y = -20;
+    }
+    if (this.x < -20) {
+        this.x = 590;
+    }
+    if (this.y < -20) {
+        this.y = 590;
+    }
 
-  if(this.ticks % 80 === 0) {
-    setTimeout(function() {
-      self.velX = -1 + Math.random() * 2;
-      self.velY = -1 + Math.random() * 2;
-    }, 2000);
-  }
+    if (this.ticks % 80 === 0) {
+        setTimeout(function() {
+            self.velX = -2 + Math.random() * 4;
+            self.velY = -2 + Math.random() * 4;
+        }, (Math.random() * 2000) + 1000);
+    }
 
-
-
-  this.x += this.velX;
-  this.y += this.velY;
+    this.x += this.velX;
+    this.y += this.velY;
 };
 
-Enemy.prototype.render = function(){
+Enemy.prototype.render = function() {
     ctx.strokeStyle = "red";
     ctx.strokeRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(this.avatar, this.x, this.y, this.width, this.height)
@@ -99,9 +94,9 @@ var enemy3 = new Enemy(100 + Math.random() * 400, 100 + Math.random() * 400);
 
 
 
-function gameLoop(){
+function gameLoop() {
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(0,0,600,600);
+    ctx.fillRect(0, 0, 600, 600);
 
     player1.update();
     player1.render();
@@ -124,13 +119,13 @@ gameLoop();
 
 
 
-document.addEventListener("keydown", function(e){
+document.addEventListener("keydown", function(e) {
     if (e.keyCode === 39) player1.velX = 5;
     if (e.keyCode === 37) player1.velX = -5;
     if (e.keyCode === 38) player1.velY = -5;
     if (e.keyCode === 40) player1.velY = 5;
 });
- document.addEventListener("keyup", function(e){
+document.addEventListener("keyup", function(e) {
     if (e.keyCode === 39) player1.velX = 0;
     if (e.keyCode === 37) player1.velX = 0;
     if (e.keyCode === 38) player1.velY = 0;
