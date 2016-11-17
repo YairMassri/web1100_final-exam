474d187b1a1fcf1bf094482f50aa421b1f3d9b57
Brenton Cozby <brenton.cozby@gmail.com>
2016-11-17 15:37:40 -0500

    put a taco in there next to the Trumps

--- Exercises/canvas-game-complete/js/main.js
+++ Exercises/canvas-game-complete/js/main.js
@@ -8,7 +8,7 @@ function Player(x, y) {
     this.height = 30;
     this.velX = 0;
     this.velY = 0;
-    this.avatar = "";
+    this.avatar = document.getElementById("player");
 }
 
 Player.prototype.update = function() {
@@ -32,6 +32,7 @@ Player.prototype.update = function() {
 Player.prototype.render = function() {
     ctx.strokeStyle = "#000";
     ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.avatar, this.x, this.y, this.width, this.height);
 }
 
 var player1 = new Player(50, 50);
@@ -82,12 +83,61 @@ Enemy.prototype.update = function() {
 Enemy.prototype.render = function() {
     ctx.strokeStyle = "red";
     ctx.strokeRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.avatar, this.x, this.y, this.width, this.height)
+    ctx.drawImage(this.avatar, this.x, this.y, this.width, this.height);
 }
 
 var enemy1 = new Enemy(100 + Math.random() * 400, 100 + Math.random() * 400);
 var enemy2 = new Enemy(100 + Math.random() * 400, 100 + Math.random() * 400);
 var enemy3 = new Enemy(100 + Math.random() * 400, 100 + Math.random() * 400);
+
+
+
+
+
+function Taco(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 60;
+    this.height = 60;
+    this.velX = -1 + Math.random() * 2;
+    this.velY = -1 + Math.random() * 2;
+    this.avatar = document.getElementById("taco");
+    this.ticks = 0;
+}
+
+Taco.prototype.update = function() {
+    this.ticks++;
+    var self = this;
+    if (this.x > 590) {
+        this.x = -20;
+    }
+    if (this.y > 590) {
+        this.y = -20;
+    }
+    if (this.x < -20) {
+        this.x = 590;
+    }
+    if (this.y < -20) {
+        this.y = 590;
+    }
+
+    if (this.ticks % 80 === 0) {
+        setTimeout(function() {
+            self.velX = -1 + Math.random() * 2;
+            self.velY = -1 + Math.random() * 2;
+        }, (Math.random() * 2000) + 1000);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+};
+
+Taco.prototype.render = function() {
+    ctx.drawImage(this.avatar, this.x, this.y, this.width, this.height);
+}
+
+var taco1 = new Taco(100 + Math.random() * 400, 100 + Math.random() * 400);
+
 
 
 
@@ -107,6 +157,9 @@ function gameLoop() {
     enemy2.render();
     enemy3.update();
     enemy3.render();
+
+    taco1.render();
+    taco1.update();
 
     window.requestAnimationFrame(gameLoop);
 }
@@ -131,3 +184,21 @@ document.addEventListener("keyup", function(e) {
     if (e.keyCode === 38) player1.velY = 0;
     if (e.keyCode === 40) player1.velY = 0;
 });
+
+
+
+var m = 0;
+var s = 0;
+function startTime() {
+    s += 1;
+    if(s >= 60) {
+        m += 1;
+        s = 0;
+    }
+
+    document.getElementById('timer').innerHTML = "minutes: " + m + "<br>seconds: " + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 1000);
+}
+startTime();
