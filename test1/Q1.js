e14dba418963bb2f81787bdcd15a5a0da620e841
Brenton Cozby <brenton.cozby@gmail.com>
2017-01-17 13:37:45 -0500

    move test1 to test1-alternative

--- test1/Q1.js
+++
@@ -1,4 +0,0 @@
-// Push an array containing 'John' and 35 as its only two values to the end of
-// the people array, using JavaScript's push method.
-
-var people = [ ['jose', 28], ['francis', 20], ['henrietta', 25] ];
