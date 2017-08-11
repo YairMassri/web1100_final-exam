6ca53647eb4ee6a5a27b185093444f300836f28e
Brenton Cozby <brenton.cozby@gmail.com>
2017-08-10 22:36:28 -0400

    update gif search

--- complete-classwork/gif-search-complete/script.js
+++ complete-classwork/gif-search-complete/script.js
@@ -1,83 +1,85 @@
-const giphy_endpoint = 'http://api.giphy.com/v1/gifs/search'
-const giphy_api_key = 'dc6zaTOxFJmzC'
+const giphy_endpoint = 'http://api.giphy.com/v1' // query key is 'q'
+const giphy_api_key = '137dce427cf745ddb322ad8d78229229' // query key is 'api_key'
 
 const searchForm = document.querySelector('#search-form')
 const searchInput = document.querySelector('#search-form input')
-const dropdown = document.querySelector('.dropdown')
 const results = document.querySelector('.results')
 
-function buildImages(data) {
-    let images = ``
+function getGifs(term, path, callback) {
+    const type = (stickers.checked === true) ? 'stickers' : 'gifs'
 
-    data.forEach(res => {
-        images += `
-            <img src="${res.images.preview_gif.url}" />
-        `
-    })
-
-    return images
-}
-
-function search(term) {
-    axios({
-        url: giphy_endpoint,
-        params: {
-            q: encodeURI(term),
-            api_key: giphy_api_key
-        }
-    })
+    axios(`${giphy_endpoint}/${type}/${path}?api_key=${giphy_api_key}&q=${term}`)
     .then(function(res) {
-        console.log(res);
+        console.log(res)
 
-        results.innerHTML = buildImages(res.data.data)
+        callback(res, type === 'stickers')
     })
 }
 
-function buildDropdown(data) {
-    let terms = ''
+function displayManyGifs(response, areStickers) {
+    for(let i = 0; i < response.data.data.length; i++) {
+        const gif_url = response.data.data[i].images.preview_gif.url
+        results.innerHTML += `
+            <a target="_blank" rel="noopener" href="${response.data.data[i].url}">
+                <img class="image ${(areStickers) ? 'sticker' : ''}" src="${gif_url}">
+            </a>
+        `
+    }
+}
 
-    data.forEach(res => {
-        terms += `
-            <span class="suggestion">${res.query}</span>
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    if(searchInput.value === '') return
+
+    results.innerHTML = ''
+
+    getGifs(searchInput.value, 'search', displayManyGifs)
+})
+
+random.addEventListener('click', function() {
+    getGifs('', 'random', function(response) {
+        results.innerHTML = `
+            <a target="_blank" rel="noopener" href="${response.data.data.url}">
+                <img class="image" src="${response.data.data.image_url}">
+            </a>
         `
     })
+})
 
-    return terms
-}
+trending.addEventListener('click', function() {
+    results.innerHTML = ''
+
+    getGifs('', 'trending', displayManyGifs)
+})
 
 // Documention for Azure Search Suggestion API:
 // https://docs.microsoft.com/en-us/rest/api/cognitiveservices/bing-autosuggest-api-v7-reference
-function predict(term) {
-    dropdown.style.display = 'block'
-    axios({
-        url: 'https://api.cognitive.microsoft.com/bing/v7.0/Suggestions',
-        params: {
-            q: term
-        },
-        headers: {
-            'Ocp-Apim-Subscription-Key': '3235ad791925462385dc69bbea9b7b7d'
-        }
-    })
-    .then(function(res) {
-        console.log(res);
-        dropdown.innerHTML = buildDropdown(res.data.suggestionGroups[0].searchSuggestions)
-    })
-}
-
-searchForm.addEventListener('submit', function(e) {
-    e.preventDefault()
-
-    search(searchInput.value)
-})
-
-searchForm.addEventListener('input', function(e) {
-    predict(searchInput.value)
-})
-
-dropdown.addEventListener('click', function(e) {
-    if(e.target.classList.contains('suggestion')) {
-        searchInput.value = e.target.textContent
-        search(e.target.textContent)
-        dropdown.style.display = 'none'
-    }
-})
+// function predict(term) {
+//     dropdown.style.display = 'block'
+//     axios({
+//         url: 'https://api.cognitive.microsoft.com/bing/v7.0/Suggestions',
+//         params: {
+//             q: term
+//         },
+//         headers: {
+//             'Ocp-Apim-Subscription-Key': '3235ad791925462385dc69bbea9b7b7d'
+//         }
+//     })
+//     .then(function(res) {
+//         console.log(res);
+//         dropdown.innerHTML = buildDropdown(res.data.suggestionGroups[0].searchSuggestions)
+//     })
+// }
+//
+// searchForm.addEventListener('input', function(e) {
+//     predict(searchInput.value)
+// })
+//
+// dropdown.addEventListener('click', function(e) {
+//     if(e.target.classList.contains('suggestion')) {
+//         searchInput.value = e.target.textContent
+//         search(e.target.textContent)
+//         dropdown.style.display = 'none'
+//     }
+// })
